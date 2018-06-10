61ad6df6ce87b5a42fbe2bf2344140b5072e8eed
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-06-09 23:44:04 -0700

    commit all

---
+++ DopeTrax/ViewModel/GroupSongs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.Web;
+
+namespace DopeTrax.ViewModel
+{
+    public class GroupSongs
+    {
+    }
+}
---
+++ addArtworkandCreator.sql
@@ -0,0 +1,6 @@
+﻿UPDATE [dbo].[Group] SET Artwork='~/Content/Images/dopetracks.png', Creator='Seth Johnson' WHERE GenreId=1
+UPDATE [dbo].[Group] SET Artwork='~/Content/Images/afrobeat.jpg', Creator='Ari Kogan' WHERE GenreId=3
+UPDATE [dbo].[Group] SET Artwork='~/Content/Images/runway.jpg', Creator='Max Barenburg' WHERE GenreId=6
+UPDATE [dbo].[Group] SET Artwork='~/Content/Images/summerdog.jpg', Creator='Aaron Poplack' WHERE GenreId=2
+UPDATE [dbo].[Group] SET Artwork='~/Content/Images/winterfell.jpg', Creator='Lindsey Johnson' WHERE GenreId=4
+UPDATE [dbo].[Group] SET Artwork='~/Content/Images/worldly.jpg', Creator='Melissa Bernstein' WHERE GenreId=5
